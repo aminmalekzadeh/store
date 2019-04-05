447181da6e200e4d62f6f499cfb127f6c865f1a2
aminmalekzadeh <aminmalekzadeh004@gmail.com>
2019-04-05 22:36:50 +0430

    hi farshid

--- category.php
+++ category.php
@@ -4,15 +4,17 @@ require_once 'function.php';
 require_once 'config.php';
 $name = $_POST['name'];
 $parent = $_POST['parent'];
-$id = $_POST['id'];
+
 try{
     $myPDO = new PDO("mysql:host=$host;dbname=$namedb",$username,$password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    $result = $myPDO->prepare('INSERT INTO category(id,name,parent) VALUES (:ID,:name,:parent)');
-    $result->bindParam(':name',$name);
-    $result->bindParam(':parent',$parent);
-    $result->bindParam(':ID',$id);
-    $result->execute();
-    e("اطلاعات با موفقیت ثبت شد.","alert-success");
+    $result = $myPDO->prepare('INSERT INTO category(NAME,PARENT) VALUES (:NAME,:PARENT)');
+    $result->bindParam(':NAME',$name);
+    $result->bindParam(':PARENT',$parent);
+    if($result->execute()){
+        e("اطلاعات با موفقیت ثبت شد.", "alert-success");
+    }else{
+        e("خطا در ذخیره اطلاعات",'alert-danger');
+    }
 }catch (PDOException $e){
     e($e->getMessage(),"alert-danger");
 }
--- category.view.php
+++ category.view.php
@@ -8,87 +8,75 @@ require_once 'section/content.php';
     <div class="main-content">
         <div class="content-wrapper">
             <div class="container-fluid">
-<section id="basic-form-layouts">
-    <div class="row">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <div class="card-title-wrap bar-success">
-                        <h4 class="card-title" id="basic-layout-form">اضافه نمودن دسته بندی</h4>
-                    </div>
-                    <p class="mb-0">اضافه نمودن دسته و زیردسته</p>
-                </div>
-
-                <div class="card-body">
-                    <div class="px-3">
-                        <form action="category.php" method="post" class="form">
-                            <div class="form-body">
-                                <h4 class="form-section">
-                                    <i class="icon-layers"></i> افزودن </h4>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="projectinput1">نام</label>
-                                            <input type="text" id="projectinput1"
-                                                   class="form-control" name="name">
-                                        </div>
+                <section id="basic-form-layouts">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-header">
+                                    <div class="card-title-wrap bar-success">
+                                        <h4 class="card-title" id="basic-layout-form">اضافه نمودن
+                                            دسته بندی</h4>
                                     </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="projectinput1"> آیدی </label>
-                                            <input type="text" id="projectinput1"
-                                                   class="form-control" name="id">
-                                        </div>
+                                    <p class="mb-0">اضافه نمودن دسته و زیردسته</p>
+                                </div>
+
+                                <div class="card-body">
+                                    <div class="px-3">
+                                        <form action="category.php" method="post" class="form">
+                                            <div class="form-body">
+                                                <h4 class="form-section">
+                                                    <i class="icon-layers"></i> افزودن </h4>
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label for="projectinput1">نام</label>
+                                                            <input type="text" id="projectinput1"
+                                                                   class="form-control" name="name">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label for="projectinput5">انتخاب</label>
+                                                            <select id="projectinput5" name="parent"
+                                                                    class="form-control text-dark">
+                                                                <option value="0">دسته اصلی</option>
+                                                                <?php
+                                                                require_once 'config.php';
+                                                                try {
+                                                                    $myPDO = new PDO("mysql:host=$host;dbname=$namedb", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                                                                    $result = $myPDO->prepare('SELECT * FROM category');
+                                                                    $result->execute();
+                                                                    $fetch = $result->fetchAll();
+                                                                    foreach ($fetch as $row) {
+
+                                                                        echo "<option value=\"" . $row['ID'] . "\">";
+                                                                        echo $row['Name'];
+                                                                        echo "</option>";
+                                                                    }
+                                                                } catch (PDOException $e) {
+                                                                    e($e->getMessage(), "alert-danger");
+                                                                }
+                                                                ?>
+
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-actions">
+                                                <button type="submit" class="btn btn-success">
+                                                    <i class="icon-note"></i> ذخیره
+                                                </button>
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
-                                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-                                <div class="row">
-                                    <div class="col-md-10">
-                                        <div class="form-group">
-                                            <label for="projectinput5">انتخاب</label>
-                                            <select  id="projectinput5" name="parent"
-                                                    class="form-control">
-                                                <option value="0">دسته اصلی</option>
-                                                <?php
-                                                require_once 'config.php';
-                                                try{
-                                                    $myPDO = new PDO("mysql:host=$host;dbname=$namedb",$username,$password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-                                                    $result = $myPDO->prepare('SELECT * FROM category');
-                                                    $result->execute();
-                                                    $fetch = $result->fetchAll();
-                                                    foreach ($fetch as $row){
-
-                                                        echo "<option value=\"$row['id'];\">";
-                                                        echo $row['Name'];
-                                                        echo "</option>";
-                                                    }
-                                                    e("اطلاعات با موفقیت ثبت شد.","alert-success");
-                                                }catch (PDOException $e){
-                                                    e($e->getMessage(),"alert-danger");
-                                                }
-                                                ?>
-
-                                            </select>
-                                        </div>
-                                    </div>
-
-
-                                </div>
                             </div>
-
-                            <div class="form-actions">
-                                <button type="submit" class="btn btn-success">
-                                    <i class="icon-note"></i> ذخیره
-                                </button>
-                            </div>
-                        </form>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+                </section>
 
-<?php
-require_once 'section/footer.php';
-?>
+                <?php
+                require_once 'section/footer.php';
+                ?>
